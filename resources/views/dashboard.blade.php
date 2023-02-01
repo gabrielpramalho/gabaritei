f9b6527790cfdef48b5af5d9df0b43037189c66e
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2023-02-01 14:33:03 -0300

    edit view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+        <h2 class="font-semibold text-l text-gray-800 leading-tight">
+            Home
         </h2>
     </x-slot>
 
@@ -10,17 +10,17 @@
         <div class="w-full px-4 py-4 mt-6 bg-white rounded-lg shadow-lg sm:w-1/2 md:w-1/2 lg:w-1/4 dark:bg-gray-800">
             <div class="flex-shrink-0">
                 <div class="flex items-center justify-center w-12 h-12 mx-auto text-white bg-emerald-600 rounded-md">
-                    <svg width="20" height="20" fill="currentColor" class="w-6 h-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
-                        </path>
-                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+                      </svg>
+                      
                 </div>
             </div>
             <h3 class="py-4 text-2xl font-semibold text-gray-700 sm:text-xl dark:text-white">
-                Website Design
+                Gerar PDF
             </h3>
             <p class="py-4 text-gray-500 text-md dark:text-gray-300">
-                Encompassing today’s website design technology to integrated and build solutions relevant to your business.
+                Flexibilidade no momento de criar seus cartões-respostas, variando em 20, 40 e 60 questões.
             </p>
         </div>
         <div class="w-full px-4 py-4 mt-6 bg-white rounded-lg shadow-lg sm:w-1/2 md:w-1/2 lg:w-1/4 sm:mt-16 md:mt-20 lg:mt-24 dark:bg-gray-800">
@@ -33,26 +33,26 @@
                 </div>
             </div>
             <h3 class="py-4 text-2xl font-semibold text-gray-700 sm:text-xl dark:text-white">
-                Branding
+                Correções
             </h3>
             <p class="py-4 text-gray-500 text-md dark:text-gray-300">
-                Share relevant, engaging, and inspirational brand messages to create a connection with your audience.
+                Corrija facilmente suas provas sem esforços, garantindo agilidade e facilidade para você.
             </p>
         </div>
         <div class="w-full px-4 py-4 mt-6 bg-white rounded-lg shadow-lg sm:w-1/2 md:w-1/2 lg:w-1/4 dark:bg-gray-800">
             <div class="flex-shrink-0">
                 <div class="flex items-center justify-center w-12 h-12 mx-auto text-white bg-emerald-600 rounded-md">
-                    <svg width="20" height="20" fill="currentColor" class="w-6 h-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
-                        </path>
-                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+                      </svg>
+                      
                 </div>
             </div>
             <h3 class="py-4 text-2xl font-semibold text-gray-700 sm:text-xl dark:text-white">
-                SEO Marketing
+                Turmas
             </h3>
             <p class="py-4 text-gray-500 text-md dark:text-gray-300">
-                Let us help you level up your search engine game, explore our solutions for digital marketing for your business.
+                O <b>Gabaritei</b> possui um sistema de turmas, em que a informação fica concetrada em apenas um local.
             </p>
         </div>
     </div>
